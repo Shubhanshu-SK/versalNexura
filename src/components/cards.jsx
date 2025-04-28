b98caf9931d61bb71b9e68fc7fb5fa80e1ab13c3
Shubhanshu-SK <shubhanshuchoudhary2006@gmail.com>
2025-04-28 19:04:46 +0530

    Text align centre

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -75,7 +75,7 @@ bottom: 10px;
     position: relative;
     width: 100%;
     height: 100%;
-    text-align: left;
+    text-align: center;
     transition: transform 0.8s;
     transform-style: preserve-3d;
   }
